fb017960e12d1b44907658710ee9ff79553bdf90
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2024-02-27 23:55:53 +0000

    add: comments for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Category, Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -63,11 +65,28 @@ def blogDetail(request, pk):
     `@return` The HTTP response. This view function renders a template to generate the HTTP response. The template displays a specific blog post.
     """
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+
+            comment.save()
+
+            return HttpResponseRedirect(request.path)
+
     comments = Comment.objects.filter(post=post)
 
     context = {
         "post": post,
         "comments": comments,
+        "form": form,
     }
 
     return render(request, "blog/detail.html", context)
