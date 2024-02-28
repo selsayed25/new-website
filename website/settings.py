038425dd626eb15913079709373181be6aa07b17
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2024-02-28 18:46:20 +0000

    fix(settings): turn debug to false

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x_^=e4!*esojl)_f+k83t4enwx-%&2^)2v-fzhmem&(e5mp4l("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
