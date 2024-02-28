650588f2fcbb54cdc2b31927c17929253bb42025
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2024-02-28 18:44:10 +0000

    fix(settings): fix static filesystem

--- website/settings.py
+++ website/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
